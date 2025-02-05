8ec7acb6719c17fb63d35b92e14ba07eee14f812
siosw <git@siosw.net>
2025-02-05 12:50:28 +0100

    init template

---
+++ README.md
@@ -0,0 +1,3 @@
+# alloy repo template
+
+Contains basic config to more easily setup new repos for the alloy projct.
---
+++ crates/example/src/main.rs
@@ -0,0 +1,11 @@
+//! Example docs for this crate
+fn main() {
+    println!("alloy!");
+}
+
+#[cfg(test)]
+mod tests {
+
+    #[test]
+    fn example_test() {}
+}
